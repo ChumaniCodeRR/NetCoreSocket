445b1349d6894c6281615299d26480d45f6d8b90
Chumani Mqulo <C.Mqulo@ru.ac.za>
2023-10-11 20:11:31 +0200

    changes

--- WebSockets_applications/Program.cs
+++ WebSockets_applications/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
